4d16439d9b11f1cf0f3a91fb6eb13fcace30da62
sridevim11 <30713926+sridevim11@users.noreply.github.com>
2017-08-14 11:06:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get('/article-one',function(req,res){
    res.send("Article one is requested") ;
 });
 app.get('/article-two',function(req,res){
-   res.send("Article one is requested") ;
+   res.send("Article two is requested") ;
 });
 app.get('/article-three',function(req,res){
-   res.send("Article one is requested") ;
+   res.send("Article three is requested") ;
 });
 
 app.get('/ui/style.css', function (req, res) {
