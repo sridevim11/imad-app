525a15b1b5785c90bcfb181bece85fdfda8c1331
sridevim11 <30713926+sridevim11@users.noreply.github.com>
2017-08-17 18:07:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,9 @@ app.get('/article-three',function(req,res){
    res.sendFile(path.join(__dirname,'ui','article-three.html')) ;
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
