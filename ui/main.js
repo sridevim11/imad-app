3b29e8dd0119a694bdb2dace5491b49bee81c324
sridevim11 <30713926+sridevim11@users.noreply.github.com>
2017-08-17 19:05:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,13 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="This is a trial app";
 var img=document.getElementById("madi");
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function() {
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
 };
 
     
