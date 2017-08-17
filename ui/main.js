573cb48829a1e6222a55276f11b3f090db511829
sridevim11 <30713926+sridevim11@users.noreply.github.com>
2017-08-17 19:06:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ element.innerHTML="This is a trial app";
 var img=document.getElementById("madi");
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function() {
