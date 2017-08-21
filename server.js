1e5a7051d1715a7a84f3972720737e9e967aef98
sridevim11 <30713926+sridevim11@users.noreply.github.com>
2017-08-21 15:47:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/counter',function(req,res){
 });
 
 var names=[];
-app.get('/submit/:name',function(req,res) {
+app.get('/submit',function(req,res) {
     
     var name=req.query.name;
     names.push(name);
