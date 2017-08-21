94f6266f916fef7b916269b2e47d94964066ac57
sridevim11 <30713926+sridevim11@users.noreply.github.com>
2017-08-21 14:54:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ click.onclick=function(){
   submit.onclick=function(){
       var names=['name1','name2','name3','name4'];
       var list='';
-      for (var i=0;names.length;i++){
+      for (var i=0;i<names.length;i++){
           list='<li>'+names[i]+'</li>';
       }
       var ul=document.getElementById("namelist");
