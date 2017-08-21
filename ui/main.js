f951074f532abd0ecbf2eb76475092c39558436b
sridevim11 <30713926+sridevim11@users.noreply.github.com>
2017-08-21 13:56:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,13 @@
 var count=0;
 var click=document.getElementById("click");
 click.onclick=function(){
-    count=count+1;
-    var span=document.getElementById("count");
-    span.innerHTML=count.toString();
-}
+    var request=new XMLHttpRequest();
+    request.onreadystaechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+            }
+    };
+  };
