38d23d5e76420ad153c1b5606a7f93d8448e72a2
sridevim11 <30713926+sridevim11@users.noreply.github.com>
2017-08-24 11:33:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,11 +24,11 @@ click.onclick=function(){
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE)
             if(request.status===200){
-               var names=request.responseText;
-               names=JSON.parse(names);
-      var list='';
-      for (var i=0;i<names.length;i++){
-          list+='<li>'+names[i]+'</li>';
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for (var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
       }
       var ul=document.getElementById("namelist");
       ul.innerHTML=list; 
